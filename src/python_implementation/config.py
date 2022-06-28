164c0c70cc306bf6f70f1fd9568557639f2aa1d2
Chavar44 <45597989+Chavar44@users.noreply.github.com>
2022-06-28 13:31:57 +0200

    Add files via upload

---
+++ src/python_implementation/config.py
@@ -0,0 +1,15 @@
+# imports
+
+# data_path = "../../data/TCGA-COAD.htseq_fpkm.tsv"
+# data_path = "/genie3/data.txt"
+data_path = "/media/sf_Projekt_BIONETS/federated-inference-of-grns/genie3/data.txt"
+
+number_trees = 500
+tree_method = "RF"
+
+Sample_size = 1000
+Genes_size=10
+number_of_hospitals = 3
+split_even = True
+# is the list of how to split uneven data: len must be of number_of_hospitals
+split_uneven = [0.1, 0.2, 0.7]
