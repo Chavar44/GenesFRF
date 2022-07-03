784d91059a6f531c03fa36fcdc3834ea5eb1ca14
Chavar44 <45597989+Chavar44@users.noreply.github.com>
2022-07-03 18:44:21 +0200

    Add files via upload

--- All/config.py
+++ All/config.py
@@ -7,8 +7,6 @@ data_path = "/media/sf_Projekt_BIONETS/federated-inference-of-grns/genie3/data.t
 number_trees = 500
 tree_method = "RF"
 
-Sample_size = 3000
-Genes_size=50
 number_of_hospitals = 3
 split_even = True
 # is the list of how to split uneven data: len must be of number_of_hospitals
