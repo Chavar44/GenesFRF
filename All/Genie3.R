4d0ac2fbf91ee827d82fa28fb090e243ae605e34
Salvador <salvador.reynoso@fau.de>
2022-07-05 13:34:19 +0200

    typo test

--- All/Genie3.R
+++ All/Genie3.R
@@ -7,7 +7,7 @@ library(GENIE3)
 set.seed(123)   #For reproducibility purposes
 
 #Read File
-path = "TCGA-COAD.htseq_fpkm.tsv"
+path = "TCGA-COAD.htseq_fpkm.tsvad"
 OGFile = read.table(path,fileEncoding="latin1",sep="\t")
 
  #transpose matrix
