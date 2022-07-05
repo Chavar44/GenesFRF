ce6700a8ff593971f3ce14cfb8e60e0ed8497bb4
Salvador <salvador.reynoso@fau.de>
2022-07-05 16:23:54 +0200

    uploaded Batch info

--- All/Batch.sh
+++ All/Batch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #
-# allocate 1 node (4 Cores) for 96 hours
-#PBS -l nodes=1:ppn=4,walltime=96:00:00
+# allocate 1 node (4 Cores) for 6 hours
+#PBS -l nodes=1:ppn=4,walltime=6:00:00
 #
 # job name 
 #PBS -N GenesFRF1
